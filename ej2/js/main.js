94cf9da4b829a7fe9ca5b2097d3da831d0b44930
cristinatato <c.tatodelatorre@gmail.com>
2019-07-20 16:18:57 +0200

    add folders and exercise 1

---
+++ ej2/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// const homeAddress = ('Paseo de Valdelasfuentes');
+
+// const homeAddress = ('Paseo del Jarama');
+
+// Da error porque es una constante
+
+let homeAddress = ('Paseo de Valdelasfuentes');
+
+homeAddress = ('Paseo del Jarama');
